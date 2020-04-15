737fbd63fbbf455ffc65e6685128ef3b6070de5f
Maxime Falaize <pro@maxime-falaize.fr>
2020-04-15 19:16:11 +0200

    feat(crypto): Remove crypto controller which is not needed anymore
    Replaced by each BankServiceInterface implementations.

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,8 @@
 <?php
 
 use ProxyBank\Controllers\BankController;
-use ProxyBank\Controllers\CryptoController;
 use Slim\Routing\RouteCollectorProxy;
 
-$app->post('/encrypt', CryptoController::class . ":encrypt");
 $app->group("/bank", function (RouteCollectorProxy $group) {
     $group->get("/list", BankController::class . ":listBanks");
     $group->post("/token", BankController::class . ":getAuthToken");
