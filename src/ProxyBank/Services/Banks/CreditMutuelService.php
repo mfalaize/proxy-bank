f43ee187092329f6d7235082150f56b5af383c07
Maxime Falaize <pro@maxime-falaize.fr>
2020-04-15 19:13:50 +0200

    feat(credit-mutuel): Add beginning of dsp2 treatment in case of valid login/password

--- src/ProxyBank/Services/Banks/CreditMutuelService.php
+++ src/ProxyBank/Services/Banks/CreditMutuelService.php
@@ -4,17 +4,36 @@
 namespace ProxyBank\Services\Banks;
 
 
+use DOMDocument;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
 use ProxyBank\Models\Bank;
 use ProxyBank\Models\Input;
 use ProxyBank\Models\TokenResult;
 use ProxyBank\Services\BankServiceInterface;
+use ProxyBank\Services\CryptoService;
 use Psr\Container\ContainerInterface;
 
 class CreditMutuelService implements BankServiceInterface
 {
 
+    const LOGIN_INPUT = "Login";
+    const PASSWORD_INPUT = "Password";
+    const TRANSACTION_ID_INPUT = "transactionId";
+
+    const DOMAIN = "www.creditmutuel.fr";
+    const BASE_URL = 'https://' . self::DOMAIN;
+    const AUTH_URL = '/fr/authentification.html';
+    const VALIDATION_URL = '/fr/banque/validation.aspx';
+
+    public $handlerStack;
+
+    private $cryptoService;
+
     public function __construct(ContainerInterface $container)
     {
+        $this->handlerStack = HandlerStack::create();
+        $this->cryptoService = $container->get(CryptoService::class);
     }
 
     public function getBank(): Bank
@@ -23,18 +42,70 @@ class CreditMutuelService implements BankServiceInterface
         $bank->id = "credit-mutuel";
         $bank->name = "Crédit Mutuel";
         $bank->authInputs = [
-            new Input("Login", Input::TYPE_TEXT),
-            new Input("Password", Input::TYPE_PASSWORD),
+            new Input(self::LOGIN_INPUT, Input::TYPE_TEXT),
+            new Input(self::PASSWORD_INPUT, Input::TYPE_PASSWORD),
         ];
         return $bank;
     }
 
     public function getAuthToken(array $inputs): TokenResult
     {
-        // TODO
         $token = new TokenResult();
-        $token->message = "Not implemented yet";
+
+        if (!isset($inputs[self::LOGIN_INPUT])) {
+            $token->message = self::LOGIN_INPUT . " value is required";
+            return $token;
+        }
+
+        if (!isset($inputs[self::PASSWORD_INPUT])) {
+            $token->message = self::PASSWORD_INPUT . " value is required";
+            return $token;
+        }
+
+        $client = $this->buildClientHttp();
+
+        $client->post(self::AUTH_URL, [
+            "form_params" => ([
+                "_cm_user" => $inputs[self::LOGIN_INPUT],
+                "_cm_pwd" => $inputs[self::PASSWORD_INPUT],
+                "flag" => "password"
+            ])
+        ]);
+
+        $response = $client->get(self::VALIDATION_URL);
+        $htmlString = (string)$response->getBody();
+
+        $matches = [];
+        preg_match("/.*transactionId: '(.+)'/", $htmlString, $matches);
+        $transactionId = $matches[1];
+
+        $html = new DOMDocument();
+        $html->loadHTML($htmlString, LIBXML_NOWARNING | LIBXML_NOERROR);
+        $message = $html->getElementById("inMobileAppMessage")->textContent;
+        $message = trim($message);
+        $message = str_replace("  ", "", $message);
+
+        $tokenJson = json_encode([
+            "bankId" => $this->getBank()->id,
+            self::LOGIN_INPUT => $inputs[self::LOGIN_INPUT],
+            self::PASSWORD_INPUT => $inputs[self::PASSWORD_INPUT],
+            self::TRANSACTION_ID_INPUT => $transactionId
+        ]);
+
+        $token->token = $this->cryptoService->encrypt($tokenJson);
+        $token->completedToken = false;
+        $token->message = $message;
         return $token;
+    }
+
+    public function buildClientHttp()
+    {
+        return new Client([
+            "base_uri" => self::BASE_URL,
+            "allow_redirects" => false,
+            "cookies" => true,
+            "handler" => $this->handlerStack
+        ]);
     }
 
     public function fetchTransactions(string $accountId): array
--- tests/Unit/Services/Banks/CreditMutuelServiceTest.php
+++ tests/Unit/Services/Banks/CreditMutuelServiceTest.php
@@ -4,21 +4,76 @@
 namespace Tests\Unit\Services\Banks;
 
 
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 use ProxyBank\Models\Input;
-use ProxyBank\Models\Security\AuthenticationStrategy;
+use ProxyBank\Models\TokenResult;
 use ProxyBank\Services\Banks\CreditMutuelService;
+use ProxyBank\Services\CryptoService;
 use Psr\Container\ContainerInterface;
 
 class CreditMutuelServiceTest extends TestCase
 {
     private $service;
 
+    private $mockResponses;
+    private $transactionsHistory;
+    private $cryptoService;
+
     protected function setUp(): void
     {
-        $this->service = new CreditMutuelService(
-            $this->createMock(ContainerInterface::class)
+        $this->cryptoService = $this->createMock(CryptoService::class);
+
+        $container = $this->createMock(ContainerInterface::class);
+        $container->expects($this->atLeastOnce())
+            ->method("get")
+            ->with(CryptoService::class)
+            ->willReturn($this->cryptoService);
+
+        $this->service = new CreditMutuelService($container);
+
+        $this->mockResponses = new MockHandler();
+        $this->service->handlerStack = HandlerStack::create($this->mockResponses);
+        $this->transactionsHistory = [];
+        $this->service->handlerStack->push(Middleware::history($this->transactionsHistory));
+    }
+
+    private function scenario_login_success_without_dsp2_cookie(): TokenResult
+    {
+        $this->mockResponses->append(
+            new Response(302, ["Set-Cookie" => "IdSes=token; Path=/; Secure"]), // login success
+            new Response(200, [], "
+<html>
+<head><meta charset=\"UTF-8\"/></head>
+<body>
+<script type=\"text/javascript\">
+var otpInMobileAppParameters = {
+	transactionId: 'aTransactionId'
+};
+</script>
+<div id=\"inMobileAppMessage\"> <h2 class=\"c otpSecuringNeeded\"> <img src=\"#\"> Votre connexion nécessite une sécurisation. </h2> <br/> <h2 class=\"c otpFontSizeIncreased\">Démarrez votre application mobile Crédit Mutuel depuis votre appareil \"<strong>MOTO G (5S) DE M MAXIME FALAIZE</strong>\" pour vérifier et confirmer votre identité.</h2><br> <br> <img src=\"#\"></div>
+</body>
+</html>
+            ") // get validation page
         );
+
+        $this->cryptoService->expects($this->atLeastOnce())
+            ->method("encrypt")
+            ->with(json_encode([
+                "bankId" => "credit-mutuel",
+                "Login" => "myLogin",
+                "Password" => "myPassword",
+                "transactionId" => "aTransactionId"
+            ]))
+            ->willReturn("encryptedToken");
+
+        return $this->service->getAuthToken([
+            "Login" => "myLogin",
+            "Password" => "myPassword"
+        ]);
     }
 
     /**
@@ -34,5 +89,68 @@ class CreditMutuelServiceTest extends TestCase
         $this->assertEquals("Password", $bank->authInputs[1]->name);
         $this->assertEquals(Input::TYPE_PASSWORD, $bank->authInputs[1]->type);
         $this->assertEquals(2, sizeof($bank->authInputs));
+    }
+
+    /**
+     * @test
+     */
+    public function getAuthToken_should_return_error_message_when_no_login()
+    {
+        $token = $this->service->getAuthToken([]);
+        $this->assertNull($token->token);
+        $this->assertNull($token->completedToken);
+        $this->assertEquals("Login value is required", $token->message);
+    }
+
+    /**
+     * @test
+     */
+    public function getAuthToken_should_return_error_message_when_no_password()
+    {
+        $token = $this->service->getAuthToken([
+            "Login" => "myLogin"
+        ]);
+        $this->assertNull($token->token);
+        $this->assertNull($token->completedToken);
+        $this->assertEquals("Password value is required", $token->message);
+    }
+
+    /**
+     * @test
+     */
+    public function getAuthToken_should_post_login_password_to_auth_url()
+    {
+        $this->scenario_login_success_without_dsp2_cookie();
+
+        $request1 = $this->transactionsHistory[0]["request"];
+        $this->assertEquals("POST", $request1->getMethod());
+        $this->assertEquals("https://www.creditmutuel.fr/fr/authentification.html", (string)$request1->getUri());
+        $this->assertEquals("application/x-www-form-urlencoded", $request1->getHeaderLine("Content-Type"));
+        $this->assertEquals("_cm_user=myLogin&_cm_pwd=myPassword&flag=password", (string)$request1->getBody());
+    }
+
+    /**
+     * @test
+     */
+    public function getAuthToken_should_request_validation_page_after_login_success_and_no_dsp2_cookie()
+    {
+        $this->scenario_login_success_without_dsp2_cookie();
+
+        $request2 = $this->transactionsHistory[1]["request"];
+        $this->assertEquals("GET", $request2->getMethod());
+        $this->assertEquals("https://www.creditmutuel.fr/fr/banque/validation.aspx", (string)$request2->getUri());
+        $this->assertEquals("IdSes=token", $request2->getHeaderLine("Cookie"));
+    }
+
+    /**
+     * @test
+     */
+    public function getAuthToken_should_return_encrypted_token_with_transactionId_from_validation_page_when_no_dsp2_cookie()
+    {
+        $token = $this->scenario_login_success_without_dsp2_cookie();
+
+        $this->assertEquals("encryptedToken", $token->token);
+        $this->assertFalse($token->completedToken);
+        $this->assertEquals("Votre connexion nécessite une sécurisation. Démarrez votre application mobile Crédit Mutuel depuis votre appareil \"MOTO G (5S) DE M MAXIME FALAIZE\" pour vérifier et confirmer votre identité.", $token->message);
     }
 }
